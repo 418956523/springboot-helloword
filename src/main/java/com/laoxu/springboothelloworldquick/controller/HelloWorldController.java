8ed2c30d724840c4a11324192d11bf21d714d960
ybob <ybob@ceibs.edu>
2020-12-16 18:21:22 +0800

    3

--- src/main/java/com/laoxu/springboothelloworldquick/controller/HelloWorldController.java
+++ src/main/java/com/laoxu/springboothelloworldquick/controller/HelloWorldController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @RequestMapping("/hello")
     public String hello(){
-        return "你好！jar包部署成功2！";
+        return "你好！jar包部署成功3！";
         //456
         //111
     }
